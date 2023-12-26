2fcde4a1952dc7b0658e0b245f217676a8517c70
martinlarosa27 <martinlarosa27@gmail.com>
2023-12-26 16:31:52 -0300

    React Navigation

---
+++ client/screens/RestaurantScreen.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function RestaurantScreen() {
+  return (
+    <SafeAreaView>
+      <Text className={"text-red-500"}>RestaurantScreen</Text>
+    </SafeAreaView>
+  );
+}
